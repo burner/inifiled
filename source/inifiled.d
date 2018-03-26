75d5623384f62e9d2f0dd7f921453ec1f71afb49
Hackerpilot <briancschott@gmail.com>
2018-03-26 01:32:44 -0700

    Allow saving of INI files with customized names

--- source/inifiled.d
+++ source/inifiled.d
@@ -8,30 +8,32 @@ string genINIparser(T)() {
 
 struct INI {
 	string msg;
+	string name;
 
-	static INI opCall(string s) {
+	static INI opCall(string s, string name = null) {
 		INI ret;
 		ret.msg = s;
+		ret.name = name;
 
 		return ret;
 	}
 }
 
-string getINI(T)() @trusted {
+INI getINI(T)() @trusted {
 	import std.traits : hasUDA;
 	foreach(it; __traits(getAttributes, T)) {
 		if(hasUDA!(T, INI)) {
-			return it.msg;
+			return it;
 		}
 	}
 	assert(false);
 }
 
-string getINI(T, string mem)() @trusted {
+INI getINI(T, string mem)() @trusted {
 	import std.traits : hasUDA;
 	foreach(it; __traits(getAttributes, __traits(getMember, T, mem))) {
 		if(hasUDA!(__traits(getMember, T, mem), INI)) {
-			return it.msg;
+			return it;
 		}
 	}
 	assert(false, mem);
@@ -50,7 +52,7 @@ string buildStructParser(T)() {
 			isBasicType!(typeof(__traits(getMember, T, it))) ||
 			isSomeString!(typeof(__traits(getMember, T, it))))
 		) {
-			ret ~= 
+			ret ~=
 				"case \"%s\": t.%s = to!typeof(t.%s)(it); break; %s"
 				.format(it, it, it, "\n");
 		}
@@ -178,13 +180,13 @@ string buildSectionParse(T)() @safe {
 	string[] ret;
 
 	foreach(it; __traits(allMembers, T)) {
-		if(hasUDA!(__traits(getMember, T, it), INI) 
-			&& !isBasicType!(typeof(__traits(getMember, T, it))) 
-			&& !isSomeString!(typeof(__traits(getMember, T, it))) 
+		if(hasUDA!(__traits(getMember, T, it), INI)
+			&& !isBasicType!(typeof(__traits(getMember, T, it)))
+			&& !isSomeString!(typeof(__traits(getMember, T, it)))
 			&& !isArray!(typeof(__traits(getMember, T, it))))
 		{
 			ret ~= ("case \"%s\": { line = readINIFileImpl" ~
-					"(t.%s, input, deaph+1); } ").
+					"(t.%s, input, depth+1); } ").
 				format(fullyQualifiedName!(typeof(__traits(getMember, T, it))),
 					it
 				);
@@ -206,13 +208,13 @@ string buildValueParse(T)() @safe {
 	string ret = "switch(getKey(line)) { // " ~ fullyQualifiedName!T ~ "\n";
 
 	foreach(it; __traits(allMembers, T)) {
-		if(hasUDA!(__traits(getMember, T, it), INI) && (isBasicType!(typeof(__traits(getMember, T, it))) 
+		if(hasUDA!(__traits(getMember, T, it), INI) && (isBasicType!(typeof(__traits(getMember, T, it)))
 			|| isSomeString!(typeof(__traits(getMember, T, it)))))
 		{
 			ret ~= ("case \"%s\": { t.%s = to!(typeof(t.%s))("
 				~ "getValue(line)); break; }\n").format(it, it, it);
-		} else if(hasUDA!(__traits(getMember, T, it), INI) 
-				&& isArray!(typeof(__traits(getMember, T, it)))) 
+		} else if(hasUDA!(__traits(getMember, T, it), INI)
+				&& isArray!(typeof(__traits(getMember, T, it))))
 		{
 			ret ~= ("case \"%s\": { t.%s = to!(typeof(t.%s))("
 				~ "getValueArray(line).split(',')); break; }\n").format(it, it, it);
@@ -222,8 +224,8 @@ string buildValueParse(T)() @safe {
 	return ret ~ "default: break;\n}\n";
 }
 
-string readINIFileImpl(T,IRange)(ref T t, ref IRange input, int deaph = 0)
-		if(isInputRange!IRange) 
+string readINIFileImpl(T,IRange)(ref T t, ref IRange input, int depth = 0)
+		if(isInputRange!IRange)
 {
 	import std.conv : to;
 	import std.string : split;
@@ -233,7 +235,7 @@ string readINIFileImpl(T,IRange)(ref T t, ref IRange input, int deaph = 0)
 		import std.stdio : writefln;
 	}
 	debug {
-		writefln("%*s%d %s %x", deaph, "", __LINE__, fullyQualifiedName!(typeof(t)),
+		writefln("%*s%d %s %x", depth, "", __LINE__, fullyQualifiedName!(typeof(t)),
 			cast(void*)&input);
 	}
 	string line;
@@ -261,26 +263,26 @@ string readINIFileImpl(T,IRange)(ref T t, ref IRange input, int deaph = 0)
 			continue;
 		}
 		debug {
-			writefln("%*s%d %s %s %b", deaph, "", __LINE__, line, fullyQualifiedName!T, 
+			writefln("%*s%d %s %s %b", depth, "", __LINE__, line, fullyQualifiedName!T,
 				isSection(line));
 		}
 
 		if(isSection(line) && getSection(line) != fullyQualifiedName!T) {
 			debug {
 				//pragma(msg, buildSectionParse!(T));
-				writefln("%*s%d %s", deaph, "", __LINE__, getSection(line));
-				writefln("%*s%d %x", deaph, "", __LINE__, 
+				writefln("%*s%d %s", depth, "", __LINE__, getSection(line));
+				writefln("%*s%d %x", depth, "", __LINE__,
 					cast(void*)&input);
 			}
-			
+
 			mixin(buildSectionParse!(T));
 		} else if(isKeyValue(line)) {
 			debug {
 				//pragma(msg, buildValueParse!(T));
-				writefln("%*s%d %s %s", deaph, "", __LINE__, getKey(line), 
+				writefln("%*s%d %s %s", depth, "", __LINE__, getKey(line),
 					getValue(line));
 			}
-			
+
 			mixin(buildValueParse!(T));
 		}
 	}
@@ -288,7 +290,7 @@ string readINIFileImpl(T,IRange)(ref T t, ref IRange input, int deaph = 0)
 	return line;
 }
 
-void writeComment(ORange,IRange)(ORange orange, IRange irange) @trusted 
+void writeComment(ORange,IRange)(ORange orange, IRange irange) @trusted
 	if(isOutputRange!(ORange, ElementType!IRange) && isInputRange!IRange)
 {
 	size_t idx = 0;
@@ -307,7 +309,7 @@ void writeComment(ORange,IRange)(ORange orange, IRange irange) @trusted
 	orange.put('\n');
 }
 
-void writeValue(ORange,T)(ORange orange, string name, T value) @trusted 
+void writeValue(ORange,T)(ORange orange, string name, T value) @trusted
 	if(isOutputRange!(ORange, string))
 {
 	import std.traits : isArray, isSomeString;
@@ -317,7 +319,7 @@ void writeValue(ORange,T)(ORange orange, string name, T value) @trusted
 		foreach(idx, it; value) {
 			if(idx != 0) {
 				orange.put(',');
-			} 
+			}
 			orange.formattedWrite("%s", it);
 		}
 		orange.formattedWrite("\"");
@@ -336,7 +338,7 @@ string removeFromLastPoint(string input) @safe {
 	}
 }
 
-void writeValues(ORange,T)(ORange oRange, string name, T value) @trusted 
+void writeValues(ORange,T)(ORange oRange, string name, T value) @trusted
 	if(isOutputRange!(ORange, string))
 {
 	import std.traits : isBasicType, isSomeString;
@@ -346,7 +348,7 @@ void writeValues(ORange,T)(ORange oRange, string name, T value) @trusted
 		foreach(idx, it; value) {
 			if(idx != 0) {
 				oRange.put(',');
-			} 
+			}
 			oRange.formattedWrite("%s", it);
 		}
 		oRange.put('"');
@@ -366,31 +368,48 @@ void writeINIFile(T)(ref T t, string filename) @trusted {
 	writeINIFileImpl(t, oRange, true);
 }
 
-void writeINIFileImpl(T,ORange)(ref T t, ORange oRange, bool section) 
-		@trusted 
+void writeINIFileImpl(T,ORange)(ref T t, ORange oRange, bool section)
+		@trusted
 {
 	import std.traits : getUDAs, hasUDA, Unqual, isArray, isBasicType,
 		   isSomeString;
 	import std.format : formattedWrite;
+	debug {
+		import std.stdio : writeln;
+	}
 	if(hasUDA!(T, INI) && section) {
-		writeComment(oRange, getINI!T());
+		writeComment(oRange, getINI!T().msg);
 	}
 
 	if(section) {
-		oRange.formattedWrite("[%s]\n", getTypeName!T);
+		if (hasUDA!(T, INI))
+		{
+			auto ini = getINI!(T);
+			if (ini.name !is null)
+				oRange.formattedWrite("[%s]\n", ini.name);
+			else
+				oRange.formattedWrite("[%s]\n", getTypeName!T);
+		}
+		else
+			oRange.formattedWrite("[%s]\n", getTypeName!T);
 	}
 
 	foreach(it; __traits(allMembers, T)) {
 		if(hasUDA!(__traits(getMember, T, it), INI)) {
 			static if(isBasicType!(typeof(__traits(getMember, T, it))) ||
-				isSomeString!(typeof(__traits(getMember, T, it)))) 
+				isSomeString!(typeof(__traits(getMember, T, it))))
 			{
-				writeComment(oRange, getINI!(T,it));
-				writeValue(oRange, it, __traits(getMember, t, it));
+				auto ini = getINI!(T,it);
+				auto name = ini.name is null ? it : ini.name;
+				debug writeln(">>>", it, " ", ini.name, " ", name);
+				writeComment(oRange, ini.msg);
+				writeValue(oRange, name, __traits(getMember, t, it));
 			} else static if(isArray!(typeof(__traits(getMember, T, it)))) {
-				writeComment(oRange, getINI!(T,it));
-				writeValues(oRange, getTypeName!T ~ "." ~ it, 
-					__traits(getMember, t, it));
+				auto ini = getINI!(T,it);
+				auto name = getTypeName!T ~ "." ~ (ini.name is null ? it : ini.name);
+				debug writeln(">>>", it, " ", ini.name, " ", name);
+				writeComment(oRange, ini.msg);
+				writeValues(oRange, name, __traits(getMember, t, it));
 			//} else static if(isINI!(typeof(__traits(getMember, t, it)))) {
 			} else static if(hasUDA!(__traits(getMember, t, it),INI))
 			{
@@ -454,7 +473,7 @@ struct Dog {
 struct Person {
 	@INI("The firstname of the Person")
 	string firstname;
-	
+
 	@INI("The lastname of the Person")
 	string lastname;
 
@@ -545,19 +564,19 @@ unittest {
 		writefln("Spose equal %b", p2.spose == p3.spose);
 		writefln("Dog equal %b", p2.dog == p3.dog);
 		assert(false);
-	}	
+	}
 	if(p != p3) {
 		writefln("\n%s\n%s", p, p3);
 		writefln("Spose equal %b", p.spose == p3.spose);
 		writefln("Dog equal %b", p.dog == p3.dog);
 		assert(false);
-	}	
+	}
 	if(p != p2) {
 		writefln("\n%s\n%s", p, p2);
 		writefln("Spose equal %b", p.spose == p2.spose);
 		writefln("Dog equal %b", p.dog == p2.dog);
 		assert(false);
-	}	
+	}
 }
 
 version(unittest) {
@@ -596,4 +615,19 @@ unittest {
 	assert(config.style_check == "disabled");
 	assert(config.multi_line == `+std.algorithm -std.foo `);
 	assert(config.filters.style_check == ["+std.algorithm"]);
+}
+
+unittest {
+	@INI("General Configuration", "general")
+	struct Configuration
+	{
+		@INI("Color of the bikeshed", "shedColor") string shedC;
+	}
+
+	Configuration c = Configuration("blue");
+	c.writeINIFile("test/bikeShed.ini");
+	Configuration c2;
+	readINIFile(c2, "test/bikeShed.ini");
+	import std.stdio;
+	writeln(c2);
 }
